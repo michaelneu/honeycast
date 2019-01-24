ba489b6d295359ea437910635023a2591251982d
Michael Neu <git@michaeln.eu>
2019-01-24 02:08:51 +0100

    add flask

---
+++ honeycast/web.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import logging
+
+httpd = Flask("honeycast")
+logger = logging.getLogger("honeycast-httpd")
+
+@httpd.after_request
+def set_custom_headers(response):
+    response.headers["Server"] = ""
+    return response
+
+@httpd.route("/setup/reboot", methods=["POST"])
+def reboot():
+    return "", 200
+
+@httpd.route("/setup/eureka_info", methods=["GET"])
+def eureka_info():
+    return jsonify({
+        "name": "ThisIsName",
+        "detail": {
+            "model_name": "ThisIsModelName",
+            "manufacturer": "JJ"
+        }
+    })
+
+@httpd.route("/", defaults={ "path": "" })
+@httpd.route("/<path:path>")
+def catch_all(path):
+    logger.info("not implemented route: %s", path)
+    return "", 200
+
+if __name__ == "__main__":
+    httpd.run(debug=True)
