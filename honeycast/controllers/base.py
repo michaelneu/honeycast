81df055dd1e398a266daf378ac22deca17668865
Michael Neu <git@michaeln.eu>
2019-02-05 23:27:30 +0100

    refactor app id and namespaces to classes

--- honeycast/controllers/base.py
+++ honeycast/controllers/base.py
@@ -1,8 +1,18 @@
 from . import Message
+from uuid import uuid4
 
 class BaseController:
+    @classmethod
+    def app_id(cls):
+        return None
+
+    @classmethod
+    def app_information(cls, session_id):
+        return []
+
     def __init__(self, namespace):
         self._namespace = namespace
+        self._session_id = str(uuid4())
 
     def matches(self, namespace):
         return self._namespace == namespace
